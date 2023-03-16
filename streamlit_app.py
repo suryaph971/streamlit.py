2fad5c91f4cb64f2e9ef9b247cb694745fac02e8
Surya Panchanapu <64318123+suryaph971@users.noreply.github.com>
2023-03-16 17:59:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 streamlit.title("My Parents New Healthy Diet")
 streamlit.header(" BreakFast Menu")
 streamlit.text("🥣 Omega 3 & Blueberry oat meal")
@@ -6,3 +7,6 @@ streamlit.text("🥗 Kale, Spinach")
 streamlit.text("🐔 Hard-boiled")
 streamlit.text("🥑🍞 Avacdo")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruits_list)
