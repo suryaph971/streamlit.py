1f1a43f2f85342fc1864b07aebc4530a0e479e74
Surya Panchanapu <64318123+suryaph971@users.noreply.github.com>
2023-03-16 17:39:03 +0530

    Update streamlit_app.py
    Added title,Header and some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diet")
+streamlit.header("BreakFast Menu")
+streamlit.text("Omega 3 & Blueberry oat meal")
+streamlit.text("Kale, Spinach")
+streamlit.text("Hard-boiled")
