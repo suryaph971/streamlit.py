bdcb9d019768d8b6a07aaf84a01dec2cbf78646a
Surya Panchanapu <64318123+suryaph971@users.noreply.github.com>
2023-03-24 00:54:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 
-fruityvice_normalized = pd.normalized_json(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 pd.DataFrame(fruityvice_normalized)
